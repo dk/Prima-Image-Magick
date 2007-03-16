0a06b200e77f22ad8ca58b40858401c407406472
dk <dk>
2007-03-16 18:16:00 +0000

    - kill junk

--- mag.h
+++ mag.h
@@ -1,14 +1,8 @@
-/* $Id: mag.h,v 1.1 2007/03/16 17:16:14 dk Exp $ */
+/* $Id: mag.h,v 1.2 2007/03/16 18:16:00 dk Exp $ */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-Image
-  *SetupList(pTHX_ SV *,SV ***,ExceptionInfo *);
-
-Image *GetList(pTHX_ SV *reference,SV ***reference_vector,long *current,
-  long *last,ExceptionInfo *exception);
 
 void
 magick_croak( char * what, ExceptionInfo * exception );

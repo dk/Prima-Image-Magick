239ca47b9cb8bd5eb8fbcc5e7981e5f46abc7519
dk <dk>
2008-07-02 09:26:20 +0000

    - v0.05

--- lib/Prima/Image/Magick.pm
+++ lib/Prima/Image/Magick.pm
@@ -1,4 +1,4 @@
-# $Id: Magick.pm,v 1.4 2008/01/02 21:16:54 dk Exp $
+# $Id: Magick.pm,v 1.5 2008/07/02 09:26:20 dk Exp $
 package Prima::Image::Magick;
 
 use strict;
@@ -7,7 +7,7 @@ require Exporter;
 our @ISA         = qw(Exporter);
 our @EXPORT_OK   = qw( prima_to_magick magick_to_prima );
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
-our $VERSION     = '0.03';
+our $VERSION     = '0.05';
 
 require XSLoader;
 XSLoader::load('Prima::Image::Magick', $VERSION);
